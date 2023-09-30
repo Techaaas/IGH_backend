c85c724fd639f060671dce73b05fedc6526dca4b
Nikolai <englia228@gmail.com>
2023-09-30 23:37:01 +0300

    Changing the logic

--- fromJSON.go
+++ fromJSON.go
@@ -27,6 +27,14 @@ type DiffData struct {
 	Files   []FileDiff `json:"files"`
 }
 
+type Content struct {
+	File  string `json:"file"`
+	Items []struct {
+		Type   string `json:"type"`
+		String string `json:"string"`
+	} `json:"items"`
+}
+
 func readJSONFromFile(filePath string) (DiffData, error) {
 	fileContent, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -52,13 +60,20 @@ func getFullCode(commit, fileName string) (string, error) {
 }
 
 func main() {
-	filePath := "diff.json" 
+	filePath := "diff.json"
 
 	diffData, err := readJSONFromFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading JSON file:", err)
 		return
 	}
+
+	// Array to store the required elements
+	var resultArray []interface{}
+
+	// Add commit1
+	resultArray = append(resultArray, diffData.Commit1)
+	resultArray = append(resultArray, diffData.Commit2)
 
 	for _, fileDiff := range diffData.Files {
 		additions := make(map[int]string)
@@ -91,25 +106,47 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("File: %s\n", fileDiff.Name)
+		var fileContent Content
+		fileContent.File = fileDiff.Name
+
 		i := 0
 		for _, line := range strings.Split(fullCode, "\n") {
 			if deletion, ok := deletions[i+1]; ok {
-				fmt.Printf("-%s\n", deletion)
+				fileContent.Items = append(fileContent.Items, struct {
+					Type   string `json:"type"`
+					String string `json:"string"`
+				}{Type: "-", String: deletion})
+
 				if addition, ok := additions[i+1]; ok {
-					fmt.Printf("+%s\n", addition)
+					fileContent.Items = append(fileContent.Items, struct {
+						Type   string `json:"type"`
+						String string `json:"string"`
+					}{Type: "+", String: addition})
+
 					i++
 					continue
 				}
 			} else if addition, ok := additions[i+1]; ok {
-				fmt.Printf("+%s\n", addition)
+				fileContent.Items = append(fileContent.Items, struct {
+					Type   string `json:"type"`
+					String string `json:"string"`
+				}{Type: "+", String: addition})
+
 				i++
 				continue
 			} else {
-				fmt.Printf("%s\n", line)
+				fileContent.Items = append(fileContent.Items, struct {
+					Type   string `json:"type"`
+					String string `json:"string"`
+				}{Type: "*", String: line})
 			}
 			i++
 		}
 
+		// Append the file content directly to the result array
+		resultArray = append(resultArray, fileContent)
+
+		fmt.Printf("Output for file %s processed\n", fileDiff.Name)
 	}
+
 }
